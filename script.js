0b4854ef2b5b6e2467ba8d148d83f7012d2d0e54
DeroMal <derrickmal123@gmail.com>
2023-11-08 12:02:13 +0300

    modified

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
+// A simple simulation of user email and total frames in the presentation
+const userEmail = 'user@example.com';
+const totalFrames = 20; // Change this to the total number of frames in the actual presentation
+let framesViewedCount = 0;
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('usageReportForm').addEventListener('submit', function(event) {
-        event.preventDefault();
-
-        // Get the values from the form fields
-        var email = document.getElementById('emailAddress').value;
-        var slide = document.getElementById('slideViewed').value;
-
-        // Construct the request payload
-        var payload = {
-            email_address: email,
-            slide_viewed: slide
-        };
-
-        // Send the data to the Cloudflare Worker endpoint
-        fetch('https://usage-report.crownzcom.workers.dev/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        })
-        .then(response => {
-            // Check the response: if the status code is 2xx, parse it as JSON
-            if (response.ok) {
-                return response.json();
-            } else {
-                // If the status code is not in the 2xx range, handle it as an error
-                // Try to read it as text, and throw it to be caught by the catch block
-                return response.text().then(text => Promise.reject(text));
-            }
-        })
-        .then(data => {
-            // Process the data from the successful JSON response
-            if (data.result === 'Success') {
-                console.log('Success:', data);
-                alert('Report submitted successfully!');
-            } else if (data.result === 'Error') {
-                // If the server indicates there was an error, display the error message
-                console.error('Error:', data.message);
-                alert(data.message);
-            }
-        })
-        .catch(error => {
-            // Handle any errors that occurred during the fetch or in the parsing of the response
-            console.error('Fetch Error:', error);
-            alert('An error occurred while submitting the report: ' + error);
+    document.querySelectorAll('.slide').forEach(function(slideButton) {
+        slideButton.addEventListener('click', function() {
+            framesViewedCount++;
+            let frameNumber = this.dataset.frameNumber;
+            logFrameView(userEmail, frameNumber, framesViewedCount, totalFrames);
         });
     });
 });
+
+function logFrameView(email, frameNumber, framesViewed, totalFrames) {
+    var payload = {
+        email_address: email,
+        frame_viewed: `Frame ${frameNumber}`,
+        no_frames_viewed: framesViewed,
+        total_no_frames: totalFrames
+    };
+
+    fetch('https://usage-report.derrickmal123.workers.dev/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result === 'Success') {
+            console.log('Frame logged successfully!');
+        } else {
+            console.error('Error logging frame:', data.message);
+        }
+    })
+    .catch((error) => {
+        console.error('An error occurred while logging the frame:', error);
+    });
+}
